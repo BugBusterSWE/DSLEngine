432d87ee25e37fe40e3616cbf709fa4f3cd9243f
korut94 <andreamnt94@gmail.com>
2016-06-01 19:37:39 +0200

    Add exception NoConnectionEstabilished

---
+++ src/utils/noConnectionEstabilished.js
@@ -0,0 +1,16 @@
+
+/**
+ * Exception throw when a token is request but the engine isn't connect with a db
+ * 
+ * @history
+ * | Name | Action performed | Date |
+ * | ---  | ---              | ---  |
+ * | Andrea Mantovani | Create class | 2016-06-01 |
+ * 
+ * @author Andrea Mantovani
+ * @license MIT
+ */
+var NoConnectionEstabilished = function () {};
+
+module.exports = NoConnectionEstabilished;
+
