37786b6ca041b387ff9be400a1f13412f0f21db8
korut94 <andreamnt94@gmail.com>
2016-06-02 17:30:29 +0200

    Remove unused array of model

--- src/token.js
+++ src/token.js
@@ -1,13 +1,20 @@
-var MaapError = require("./utils/MaapError.js");
-const EventEmitter = require('events');
-const util = require('util');
+var MaapError = require("./utils/MaapError");
+const EventEmitter = require("events");
+const util = require("util");
 
 /**
  * Set dsl's store and point to access at the any engine defined in MaaS.
  * Token inherits from EventEmitter. Any engine create own events to
  * comunicate with the other engines. The only once own event of Token is
  * "update". The event "update" is emit when the new model to be register
- * into the token.
+ * into the token. The event send the follow object : 
+ * ```
+ * {
+ *   models: Model[]
+ * }
+ * ```
+ * `status.models` is the array with the last models loaded.
+ *
  *
  * @history
  * | Name | Action performed | Date |
@@ -18,9 +25,8 @@ const util = require('util');
  * @license MIT
  */
 var Token = function () {
-    this.modelRegistry = [];
     this.status = {
-        model: []    
+        models: []    
     };
 
     EventEmitter.call(this);
@@ -30,25 +36,13 @@ util.inherits(Token, EventEmitter);
 
 /**
  * @description
- * Extract the models stored in the token.
- * @return {Model[]}
- */
-Token.prototype.extract = function () {
-    return this.modelRegistry;
-};
-
-/**
- * @description
- * Save into this token the model extracted from the dsl file and
- * send the notifies for each observer attached at the token.
+ * Send the notifies for each observer attached at the token with the model loaded.
  * @param model {Model}
  * The model to store
  */
 Token.prototype.register = function (model) {
-    this.modelRegistry.push(model);
-
     this.status.model = model;
-    this.emit("update");
+    this.emit("update", this.status);
 };
 
 module.exports = Token;
