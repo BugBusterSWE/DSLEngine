bfe21447b01d2de75cffd82dc3377d3290620143
Andrea Mantovani <andreamnt94@gmail.com>
2016-06-03 16:51:11 +0200

    Perform load DSL with token

--- src/dslEngine.js
+++ src/dslEngine.js
@@ -11,6 +11,7 @@ var NoConnectionEstabilished = require("./utils/noConnecionEstabilished");
  * @history
  * | Name | Action performed | Date |
  * | ---  | ---              | ---  |
+ * | Andrea Mantovani | The token keep the connectio to db | 2016-06-03 |
  * | Andrea Mantovani | Uso Token and remove method get | 2016-06-01 |
  * | Andrea Mantovani | Update document and correct import | 2016-05-12 |
  * | Andrea Mantovani | Create class | 2016-05-11 |
@@ -20,7 +21,6 @@ var NoConnectionEstabilished = require("./utils/noConnecionEstabilished");
  */
 var DSLEngine = function () {
     this.strategy = new DslConcreteStrategy();
-	this.db = undefined;
 };
 
 /**
@@ -31,72 +31,8 @@ var DSLEngine = function () {
  * @return {Token}
  * A token connected with this engine.
  */
-DSLEngine.prototype.createToken = function () {
-	if (this.db === undefined) {
-		throw new NoConnectionEstabilished();	
-	}
-
-	var token = new Token();
-
-	return token;
-};
-
-/**
- * @description
- * Connect to the dabase to perform the action defined in the dsl througt the
- * his URL.
- * @param database {string}
- * URL for the collection into the database to perform the action. The url is
- * in the form:
- * `<dbuser>:<dbpassword>@<address>:<port>/<database>/<collection>`
- * @return {Promise}
- * The promise of the connection to the db and initial process. The promise
- * is resolve with nothing if no errors is occurred, otherwise with the
- * errors. If an istance of DSLEngine was create, the promise is always 
- * resolve.
- */
-DSLEngine.prototype.connectTo = function (database) {
-    var self = this;
-
-    return new Promise((resolve, reject) => {
-		if (self.connection === undefined) {
-			var connection = mongoose.createConnection(
-				`mongodb://${database}`
-			);
-
-			connection.on("error", function(err) {
-				reject(err);
-			});
-
-			connection.on("open", function(ref) {
-				// Error to read the macro
-				try {
-					// Use the connection to perform the DSL
-					self.db = connection;
-				} catch (err) {
-					reject(err);
-				}
-		
-				resolve(ref);
-			});
-		} else { 
-			resolve();
-		}
-    });
-};
-
-/**
- * @description
- * Connect to the dabase to perform the action defined in the dsl through a
- * defined connection.
- * @param database {mongoose.Connection}
- * Connection for the collection into the database to perform the action
- * @throws {MaaPError}
- */
-DSLEngine.prototype.connectWith = function (connection) {
-    if (this.db === undefined) {
-		this.db = connection;
-    }
+DSLEngine.prototype.generateToken = function (db) {
+    return new Token(db);
 };
 
 /**
@@ -107,16 +43,38 @@ DSLEngine.prototype.connectWith = function (connection) {
  * The code of the dsl
  * @param token {Token} 
  * The token where to be store the dsl. 
- * @return {Token}
- * The token with the models loaded within his. If no token is passed, the DSLEngine 
- * will create a new token with the model loaded.
+ * @return {Promise<void>}
  * @throws {MaaPError}
  */
 DSLEngine.prototype.loadDSL = function (dsl, token) {
-	token = token || this.createToken();
-    token.registry(this.strategy.load(dsl));
+    return new Promise((resolve, reject) => {
+	try {
+	    var models = this.strategy.load(dsl, token.getConnection());
+	} catch (err) {
+	    // Catch the loading error
+	    reject(err);
+	}
+	
+	// Register event for the succefull loading or for catching all
+	// errors occured
+	var successLoad = () => {
+	    token.removeListener("errorLoad", errorLoad);
+	    resolve();
+	};
 
-	return token;
+	var errorLoad = (err) => {
+	    token.removeListener("successLoad", succesLoad);
+	    reject(err);
+	};
+	
+	// All model are succesfull load
+	token.once("successLoad", successLoad);
+	// Catch all errors that it arises only when the models will
+	// register into the specific engine
+	token.once("errorLoad", errorLoad);
+
+	token.registry(models);
+    });
 };
 
 module.exports = DSLEngine;
