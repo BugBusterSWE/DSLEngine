5db399c560ffd67fc65983ac33269615a4ca3686
korut94 <andreamnt94@gmail.com>
2016-06-08 11:00:20 +0200

    Add model row to swap column inside the dashboard

---
+++ src/model/DashRowModel.js
@@ -0,0 +1,35 @@
+var hash = require("object-hash");
+var AttributeReader = require("../utils/AttributeReader");
+var DocumentSchema = require("./DocumentSchema");
+var MaapError = require("../utils/MaapError");
+
+var DashRowModel = function () {
+    this.column = [];
+}
+
+DashRowModel.prototype.registerCell = function (cellReference) {
+    this.column.push({
+        type: "cell",
+        label: cellReference
+    });
+};
+
+DashRowModel.protype.registerCollection = function (collectionReference) {
+    this.column.push({
+        type: "collection",
+        label: collectionReference
+    });
+};
+
+DashRowModel.prototype.registerDocument = function (documentReference) {
+    this.column.push({
+        type: "document",
+        label: documentReference
+    });   
+};
+
+DashRowModel.prototype.swap = function (dashboard) {
+    dashboard.push(this.column);
+};
+
+module.exports = DashRowModel;
