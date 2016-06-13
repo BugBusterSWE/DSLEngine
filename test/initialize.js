fe6bd295585eebd51d66e7899f17f8a17f1dfed6
Andrea Mantovani <andreamnt94@gmail.com>
2016-06-13 23:13:36 +0200

    Add test for push token

--- test/initialize.js
+++ test/initialize.js
@@ -1,22 +1,48 @@
-var DSLEngine = require("../src/dslEngine.js");
+var env = require('node-env-file');
+env(`./.env`);
+
 var mongoose = require("mongoose");
 var chai = require("chai");
 chai.use(require("chai-as-promised"));
 
+var DSLEngine = require("../src/dslEngine");
+var TokenAlreadyInsertException = require(
+    "../src/utils/tokenAlreadyInsertException"
+);
+
 describe("Token", () => {
     var engine;
+    var token;
     var connection;
 
     before(() => {
+	console.log(process.env.CONNECTION);
 	engine = new DSLEngine();
-	connection = mongoose.createConnection(null);
     });
 
     describe("#createAToken", () => {
 	it("should return a not undefined token", () => {
-	    var token = engine.generateToken(connection);
+	    token = engine.generateToken(mongoose.createConnection(null));
 	    chai.expect(token).to.not.undefined;
 	});
     });
+
+    describe("#insertToken", () => {
+	it("shoud not throw any exception when push a token", () => {
+	    chai.expect(
+		engine.pushToken.bind(engine, token)
+	    ).to.not.throw();
+	});
+    });
+
+    describe("#insertTokenOneOtherTime", () => {
+	it("should throw exception TokenAlreadyInsertException", () => {
+	    chai.expect(engine.pushToken.bind(engine, token)).to.throw(
+		TokenAlreadyInsertException
+	    );
+	});
+    });
+
+    
 });
 
