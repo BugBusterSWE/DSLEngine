0a0610cab860417920232bfd83010e8394cd5a11
korut94 <andreamnt94@gmail.com>
2016-06-14 11:31:03 +0200

    Declare env var CONNECTION and set it in both package and test

--- test/initialize.js
+++ test/initialize.js
@@ -1,6 +1,3 @@
-var env = require('node-env-file');
-env(`./.env`);
-
 var mongoose = require("mongoose");
 var chai = require("chai");
 chai.use(require("chai-as-promised"));
@@ -16,31 +13,31 @@ describe("Token", () => {
     var connection;
 
     before(() => {
-	console.log(process.env.CONNECTION);
-	engine = new DSLEngine();
+	   console.log(process.env.npm_package_config_CONNECTION);
+	   engine = new DSLEngine();
     });
 
     describe("#createAToken", () => {
-	it("should return a not undefined token", () => {
-	    token = engine.generateToken(mongoose.createConnection(null));
-	    chai.expect(token).to.not.undefined;
-	});
+	   it("should return a not undefined token", () => {
+            token = engine.generateToken(mongoose.createConnection(null));
+	       chai.expect(token).to.not.undefined;
+	   });
     });
 
     describe("#insertToken", () => {
-	it("shoud not throw any exception when push a token", () => {
-	    chai.expect(
-		engine.pushToken.bind(engine, token)
-	    ).to.not.throw();
-	});
+	   it("shoud not throw any exception when push a token", () => {
+	       chai.expect(
+                engine.pushToken.bind(engine, token)
+	       ).to.not.throw();
+	   });
     });
 
     describe("#insertTokenOneOtherTime", () => {
-	it("should throw exception TokenAlreadyInsertException", () => {
-	    chai.expect(engine.pushToken.bind(engine, token)).to.throw(
-		TokenAlreadyInsertException
-	    );
-	});
+	   it("should throw exception TokenAlreadyInsertException", () => {
+	       chai.expect(engine.pushToken.bind(engine, token)).to.throw(
+		      TokenAlreadyInsertException
+	       );
+	   });
     });
 
     
